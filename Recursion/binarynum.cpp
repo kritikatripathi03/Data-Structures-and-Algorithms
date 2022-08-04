4623102d5a1f55d4dca2840a23dba61a14e94273
Kritika Tripathi <kritika.tripathi03@gmail.com>
2022-08-04 17:22:46 +0530

    Practicing Recursion

---
+++ Recursion/binarynum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string recursebin(long long int num, int ten){
+    string result = "";
+    if(num == 0){
+        return "0";
+    }
+    return result.push_back((num % 2) * (long long int)(pow(10, ten)+1e-9)  + recursebin(num / 2, ten + 1));
+}
+
+int main(){
+    int ten = 10;
+    cout << to_string(recursebin(786435, 0)) << endl;
+}
--- Recursion/reverse.cpp
+++ Recursion/reverse.cpp
@@ -9,7 +9,20 @@ string reverse(string s){
     return s[s.length() - 1] + reverse(s.substr(0, s.size() - 1));
 }
 
+void reverse2(string &s, int i, int j){
+    
+    if(i > j)
+        return;
+    swap(s[i], s[j]);
+    i ++;
+    j --;
+    return reverse2(s, i, j);
+}
+
 int main(){
-    cout << reverse("abcde") << endl;
+    //cout << reverse("abcde") << endl;
+    string s = "abcde";
+    reverse2(s, 0, s.size() - 1);
+    cout << s << endl;
     return 0;
 }
